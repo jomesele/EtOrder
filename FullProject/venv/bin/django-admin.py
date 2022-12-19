11b057b534b9991b8635f91425303fbe37fd4f11
jomesele <jomesele@gmail.com>
2022-12-19 03:18:05 +0300

    First commit

---
+++ FullProject/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/esp66/Desktop/foodorder/Django-Online-Food-Delivery-master/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
